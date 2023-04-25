ae65299c50a5715da95b7930586bf9ec47d36430
prizemlenie <prizemlenie@gmail.com>
2023-04-25 12:01:05 +0200

    HH-178632 export prefer import aliases rule

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import noInternalPageModules from 'rules/no-internal-modules';
 import hhImportOrder from 'rules/hh-import-order';
+import preferImportAliases from 'rules/prefer-import-aliases';
 
 export const rules = {
     "no-internal-modules": noInternalPageModules,
     "hh-import-order": hhImportOrder,
+    "prefer-import-aliases": preferImportAliases,
 };
 
 export default {};
